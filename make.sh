6f915d3333af80abae46342e02a278b58bee0002
Navneet Aman <contact@navneetaman.com>
2024-10-02 15:32:27 +0530

    refactor

--- make.sh
+++ make.sh
@@ -2,5 +2,5 @@
 cd /home/navn/workspace/rust/rrun
 gcc -Ofast rrun.c -o ~/bin/rrun
 RUSTFLAGS="-C prefer-dynamic" cargo build --release
-cp --debug -vu target/release/deps/*.so ~/bin/lib/rust
-cp --debug -vu ~/nonssd/rust/rustup/toolchains/nightly-x86_64-unknown-linux-gnu/lib/libstd-*.so ~/bin/lib/rust
+cp -vu target/release/deps/*.so ~/bin/lib/rust
+cp -vu ~/nonssd/rust/rustup/toolchains/nightly-x86_64-unknown-linux-gnu/lib/libstd-*.so ~/bin/lib/rust
