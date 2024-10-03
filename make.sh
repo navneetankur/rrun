486dfaf43987d1111d84b3e904cf25ee323e99fd
Navneet Aman <contact@navneetaman.com>
2024-10-03 22:39:47 +0530

    copy rlib files too

--- make.sh
+++ make.sh
@@ -2,5 +2,7 @@
 cd /home/navn/workspace/rust/rrun
 gcc -Ofast rrun.c -o ~/bin/rrun
 RUSTFLAGS="-C prefer-dynamic" cargo build --release
-cp -vu target/release/deps/*.so ~/bin/lib/rust
-cp -vu ~/nonssd/rust/rustup/toolchains/nightly-x86_64-unknown-linux-gnu/lib/libstd-*.so ~/bin/lib/rust
+# cargo build --release
+cp -u target/release/deps/*.so ~/bin/lib/rust
+cp -u target/release/deps/*.rlib ~/bin/lib/rust
+cp -u ~/nonssd/rust/rustup/toolchains/nightly-x86_64-unknown-linux-gnu/lib/libstd-*.so ~/bin/lib/rust
